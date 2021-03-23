b39aa893ca3c3646b959352d090cc146b787e268
diegoabustos <diego.bustos@ivoy.mx>
2021-03-23 17:26:47 -0600

    FEAT: Add files to repository

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Container from "../components/container";
+
+const About = () => {
+  return (
+    <Container>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, ratione!
+        Quam exercitationem officiis enim dolorem, natus aliquam adipisci
+        tempore consequuntur suscipit culpa quasi inventore laboriosam numquam
+        corrupti esse ab distinctio.
+      </p>
+    </Container>
+  );
+};
+
+export default About;
---
+++ pages/services.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Container from '../components/container';
+
+
+const Services = () => {
+  return (  
+    <Container>
+        <Head>
+            <title>Next.js Project - Services</title>
+        </Head>
+        <h1>Services</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolorum tempore odit laborum. Voluptates quo ipsum ullam maxime nam magnam debitis, delectus, velit cumque nobis repudiandae similique est eum eos?</p>
+    </Container>
+    
+    )
+}
+
+export default Services;
